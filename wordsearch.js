d9397c644da929bba6544469f027fddb9b5e4b8d
Adam Marx <adamalexmarx@gmail.com>
2023-01-18 19:49:45 -0800

    Added conditional for empty array

--- wordsearch.js
+++ wordsearch.js
@@ -1,34 +1,38 @@
-const wordSearch = (letters, word) => { 
-  
-    let wordIsThere = false;
+const wordSearch = (letters, word) => {
 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    // console.log('horizontalJoin', horizontalJoin)
-    for (l of horizontalJoin) {
-      if (l.includes(word)) wordIsThere = true;
-    } 
+  if (letters.length < 1) {
+    return false;
+  }
+
+  let wordIsThere = false;
+
+  const horizontalJoin = letters.map(ls => ls.join(''))
+  // console.log('horizontalJoin', horizontalJoin)
+  for (l of horizontalJoin) {
+    if (l.includes(word)) wordIsThere = true;
+  }
 
 
   let arr2 = [];
-    for(let x = 0; x < letters[0].length; x++){
-      let subArr =[];
-      arr2.push(subArr);
+  for (let x = 0; x < letters[0].length; x++) {
+    let subArr = [];
+    arr2.push(subArr);
+  }
+  for (i = 0; i < letters.length; i++) {
+    for (let j = 0; j < letters[0].length; j++) {
+      arr2[j][i] = letters[i][j];
     }
-    for (i = 0; i < letters.length; i++) {
-        for(let j =0; j < letters[0].length; j++ ){
-          arr2[j][i] = letters[i][j];
-        }
-    }
+  }
 
-    const verticalJoin = arr2.map(ls => ls.join(''))
-    // console.log('horizontalJoin', horizontalJoin)
-    for (l of verticalJoin) {
-      if (l.includes(word)) wordIsThere = true;
-    } 
-    console.log(arr2);
-    return wordIsThere;
-    
-  
+  const verticalJoin = arr2.map(ls => ls.join(''))
+  // console.log('horizontalJoin', horizontalJoin)
+  for (l of verticalJoin) {
+    if (l.includes(word)) wordIsThere = true;
+  }
+  //console.log(arr2);
+  return wordIsThere;
+
+
 }
 
 module.exports = wordSearch
